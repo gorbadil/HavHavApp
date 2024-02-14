def9d37d48c9c8b230601eac20126d025a8cdf78
gorbadil <gorbadil@gmail>
2024-02-14 14:39:58 +0300

    env changed

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.API_URL": JSON.stringify(
-      "https://vet-app-backend-vwmp.onrender.com"
-    ),
-  },
 });
