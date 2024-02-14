324365e61d19b4f7127e90deb85e80cac2349d25
gorbadil <gorbadil@gmail>
2024-02-14 14:11:55 +0300

    new version of process.env

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env.API_URL": JSON.stringify(process.env.API_URL),
+    "process.env.API_URL": JSON.stringify(
+      "https://vet-app-backend-vwmp.onrender.com"
+    ),
   },
 });
